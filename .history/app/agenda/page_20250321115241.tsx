cdf0c0d4043d37e639db5a6c45323ba01ff9b045
Bafri Schneider <productionswiss8@gmail.com>
2025-03-21 12:57:18 +0100

    first commit

---
+++ .history/app/agenda/page_20250321115241.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AgendaPageWrapper } from '@/components'
+
+
+const AgendaPage = () => {
+  return (
+    <div className='main'>
+      <AgendaPageWrapper />
+    </div>
+  )
+}
+
+export default AgendaPage
